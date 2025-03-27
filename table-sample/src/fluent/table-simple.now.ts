0d507a78185b44bad02b311dbab2011482acb76e
Bryce Godfrey <bryce.godfrey@servicenow.com>
2025-03-27 08:27:16 -0700

    fix typo

--- table-sample/src/fluent/table-simple.now.ts
+++ table-sample/src/fluent/table-simple.now.ts
@@ -6,7 +6,7 @@ import { Table, StringColumn, IntegerColumn, BooleanColumn, DateColumn } from '@
 export const x_tablesample_name = Table({
     name: 'x_tablesample_name',
     schema: {
-        string_column: StringColumn({ mandatory: true, lable: 'String Column' }),
+        string_column: StringColumn({ mandatory: true, label: 'String Column' }),
         integer_column: IntegerColumn({ mandatory: true, label: 'Integer Column' }),
         boolean_column: BooleanColumn({ mandatory: true }),
         date_column: DateColumn({ mandatory: true }),
