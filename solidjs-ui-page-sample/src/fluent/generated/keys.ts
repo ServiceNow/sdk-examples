f57f8fd5a426c3fec523b7736c3910570df263bc
wolfgang.wedemeyer <wolfgang.wedemeyer@servicenow.com>
2025-09-16 11:38:51 -0700

    update scope in generated keys

--- solidjs-ui-page-sample/src/fluent/generated/keys.ts
+++ solidjs-ui-page-sample/src/fluent/generated/keys.ts
@@ -17,12 +17,12 @@ declare global {
                         table: 'sys_module'
                         id: '28fcd69dd5f44ff4a5365be9d86f590a'
                     }
-                    'x_sepa_work2/main': {
+                    'x_solidjs_example/main': {
                         table: 'sys_ux_lib_asset'
                         id: '4b46985b614b42a18dc61a21b97b4608'
                         deleted: false
                     }
-                    'x_sepa_work2/main.js.map': {
+                    'x_solidjs_example/main.js.map': {
                         table: 'sys_ux_lib_asset'
                         id: 'decacd03dd8346559cc7a01054aca662'
                         deleted: false
@@ -33,7 +33,7 @@ declare global {
                         table: 'sys_dictionary'
                         id: '0834472b7f6e4e3eb430d3c1178da6ff'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                             element: 'description'
                         }
                     },
@@ -41,7 +41,7 @@ declare global {
                         table: 'sys_dictionary'
                         id: '1327234a925148289c4e4a538ffc63ee'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                             element: 'number'
                         }
                     },
@@ -49,7 +49,7 @@ declare global {
                         table: 'sys_number'
                         id: '351d1c06dce44538a739fb165bc86e07'
                         key: {
-                            category: 'x_sepa_work2_incident'
+                            category: 'x_solidjs_example_incident'
                             prefix: 'INC'
                         }
                     },
@@ -57,7 +57,7 @@ declare global {
                         table: 'sys_choice'
                         id: '37777476c5b54a11b18fbcf9feaa915e'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                             element: 'status'
                             value: 'new'
                         }
@@ -66,7 +66,7 @@ declare global {
                         table: 'sys_documentation'
                         id: '3dbac9f27c234b789beadc29eaa08cd2'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                             element: 'resolved_at'
                             language: 'en'
                         }
@@ -75,7 +75,7 @@ declare global {
                         table: 'sys_choice'
                         id: '510b0cb67f7542c5944d4595dfd349cb'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                             element: 'status'
                             value: 'resolved'
                         }
@@ -84,7 +84,7 @@ declare global {
                         table: 'sys_choice'
                         id: '528b9385c16044a09a33cd99c67f5045'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                             element: 'priority'
                             value: '2'
                         }
@@ -93,7 +93,7 @@ declare global {
                         table: 'sys_choice'
                         id: '5a89d6924204483f8e1df4c327281d80'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                             element: 'priority'
                             value: '1'
                         }
@@ -102,7 +102,7 @@ declare global {
                         table: 'sys_dictionary'
                         id: '5c6ca73c28dd406f9baf86559d878960'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                             element: 'opened_at'
                         }
                     },
@@ -110,7 +110,7 @@ declare global {
                         table: 'sys_documentation'
                         id: '5faaf290ab294f7c8592718750b88fe9'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                             element: 'priority'
                             language: 'en'
                         }
@@ -119,7 +119,7 @@ declare global {
                         table: 'sys_dictionary'
                         id: '62beb119411246b482de315208d48909'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                             element: 'status'
                         }
                     },
@@ -127,7 +127,7 @@ declare global {
                         table: 'sys_choice'
                         id: '6d18356686724d40ba074f3cb9bbd314'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                             element: 'status'
                             value: 'closed'
                         }
@@ -136,14 +136,14 @@ declare global {
                         table: 'sys_db_object'
                         id: '6fe0a1ec8d9a4c1db67742c0f819539d'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                         }
                     },
                     {
                         table: 'sys_documentation'
                         id: '888b84ef20c54d769ea27e60b39c5627'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                             element: 'opened_at'
                             language: 'en'
                         }
@@ -152,7 +152,7 @@ declare global {
                         table: 'sys_documentation'
                         id: '8aeb07a7ecde4fb2bcb67df8474feb9b'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                             element: 'number'
                             language: 'en'
                         }
@@ -161,7 +161,7 @@ declare global {
                         table: 'sys_dictionary'
                         id: '8e44313ae4d74be88b533d01f0aa9e2a'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                             element: 'priority'
                         }
                     },
@@ -169,14 +169,14 @@ declare global {
                         table: 'ua_table_licensing_config'
                         id: '9a9c6ec137604cc3a01e3cf9775638d8'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                         }
                     },
                     {
                         table: 'sys_documentation'
                         id: '9b960c6080f846be88e621cba90fb388'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                             element: 'description'
                             language: 'en'
                         }
@@ -185,7 +185,7 @@ declare global {
                         table: 'sys_dictionary'
                         id: '9c2b515af76a4f3080378e7fd9795ded'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                             element: 'resolved_at'
                         }
                     },
@@ -193,7 +193,7 @@ declare global {
                         table: 'sys_documentation'
                         id: 'a2c5c573dafb4c278c135d36b47d3a91'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                             element: 'NULL'
                             language: 'en'
                         }
@@ -202,7 +202,7 @@ declare global {
                         table: 'sys_documentation'
                         id: 'ba3223fedc1d4ee2aa36d6472d5efb55'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                             element: 'status'
                             language: 'en'
                         }
@@ -211,7 +211,7 @@ declare global {
                         table: 'sys_choice'
                         id: 'c13e8175c5e842b497213fec4451288d'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                             element: 'status'
                             value: 'on_hold'
                         }
@@ -220,7 +220,7 @@ declare global {
                         table: 'sys_documentation'
                         id: 'c29853b0b7bd4aaa8994c5d57ea6ed5e'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                             element: 'short_description'
                             language: 'en'
                         }
@@ -229,7 +229,7 @@ declare global {
                         table: 'sys_dictionary'
                         id: 'cbb7c7a72d7e4c24a112968ad49965d8'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                             element: 'short_description'
                         }
                     },
@@ -237,7 +237,7 @@ declare global {
                         table: 'sys_choice'
                         id: 'e4d59e7fd59e41cc902c2d14cb81c01d'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                             element: 'priority'
                             value: '4'
                         }
@@ -246,7 +246,7 @@ declare global {
                         table: 'sys_dictionary'
                         id: 'e9a490b58d6f4245b719a110e38168e1'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                             element: 'NULL'
                         }
                     },
@@ -254,7 +254,7 @@ declare global {
                         table: 'sys_choice'
                         id: 'f33dff1e0d9c483abe1d3078fff20dd1'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                             element: 'status'
                             value: 'in_progress'
                         }
@@ -263,7 +263,7 @@ declare global {
                         table: 'sys_choice'
                         id: 'fd68f35da42f4d1cb2c4d80847922627'
                         key: {
-                            name: 'x_sepa_work2_incident'
+                            name: 'x_solidjs_example_incident'
                             element: 'priority'
                             value: '3'
                         }
